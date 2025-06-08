ac30736d24e7d5a1a6d5075b66d0e6b3f0a84559
Akash <shelkea48@gmail.com>
2025-06-08 20:38:44 +0530

    10th june Task for print by using formatiing  (Sysytem.out,prinf("%d",a) table of 9

---
+++ src/ex_01_Java_Basic/Lab_10th_june_task.java
@@ -0,0 +1,30 @@
+package ex_01_Java_Basic;
+
+public class Lab_10th_june_task {
+
+    public static void main(String[] args) {
+        int a=9;
+        int b=1;
+        System.out.printf("%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+        System.out.printf("\n%d * %d=%d",a,b,a*b);
+        b++;
+
+    }
+}
